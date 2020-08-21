e7962d01b8787e3b532ab6b4f4511766e72d7a2d
tianjingjing1 <jingjing1@eastmoney.com>
2020-08-21 15:28:39 +0800

    pushTest

--- src/pages/msite/msite.vue
+++ src/pages/msite/msite.vue
@@ -1,15 +1,15 @@
 <template>
 <html id="msite">
   <headTop signinUp="true">
-    <!-- <router-link :to="/search/geohash" class="link_search" slot="search">
+    <router-link to="/search" class="link_search" slot="search">
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
         <circle cx="8" cy="8" r="7" stroke="rgb(255,255,255)" stroke-width="1" fill="none" />
         <line x1="14" y1="14" x2="20" y2="20" style="stroke:rgb(255,255,255);stroke-width:2" />
       </svg>
-    </router-link>-->
-    <!-- <router-link :to="home" slot="changecity">
+    </router-link>
+    <router-link to="/home" slot="changecity">
       <p>{{headerAddress}}</p>
-    </router-link>-->
+    </router-link>
   </headTop>
   <footerGuide></footerGuide>
 </html>
@@ -20,17 +20,27 @@ import headTop from "../../components/header/head";
 import footerGuide from "../../components/footer/footGuide";
 // import { mapState, mapMutations } from "vuex";
 import { imgBaseUrl } from "../../../src/config/env";
+import { cityGuess } from "../../service/getData";
 // import func from "../../../vue-temp/vue-editor-bridge";
 export default {
   components: { headTop, footerGuide },
   data() {
     return {
-      headerAddress: "123"
+      headerAddress: "123",
+      geohash: ""
     };
   },
 
   mounted: function() {
     console.log(this.$router.query);
+    if (!this.$router.query) {
+      cityGuess().then(res => {
+        console.log(res);
+        this.headerAddress = res.name;
+      });
+    } else {
+      this.geohash = this.$router.query.geohash;
+    }
   }
 };
 </script>
